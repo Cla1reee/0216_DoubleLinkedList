a545b05043a9737d9179e7f3d1fe98c8436602b9
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-05-02 15:47:51 +0700

    membuat input output Node mahasiswa

--- 0216_DoubleLinkedList.cpp
+++ 0216_DoubleLinkedList.cpp
@@ -12,5 +12,11 @@ struct node {
 Node* START = NULL;
 
 void addNode(){
+    Node* newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
     
 }
